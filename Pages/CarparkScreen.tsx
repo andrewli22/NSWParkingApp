9c6cad8ae7644565c8caee854fb38bab11cdf93b
Andrew Li <andrew.li2277@gmail.com>
2024-11-17 21:17:02 +1100

    Added pie chart for visuals

--- Pages/CarparkScreen.tsx
+++ Pages/CarparkScreen.tsx
@@ -1,19 +1,24 @@
-import { View, Text, RefreshControl, ScrollView, StyleSheet } from "react-native"
+import { View, Text, RefreshControl, ScrollView, StyleSheet } from 'react-native'
 import { RootStackParamList } from '../utils/types';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { useCallback, useEffect, useState } from "react";
-import { API_KEY } from "../config";
-import { URL } from "../utils/api";
-import { SafeAreaView } from "react-native-safe-area-context";
-
+import { useCallback, useEffect, useState } from 'react';
+import { API_KEY } from '../config';
+import { URL } from '../utils/api';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { PieChart } from 'react-native-gifted-charts';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Carpark'>;
 
 export const CarparkScreen = ({ route }: Props) => {
-  const { facilityId, facilityName } = (route.params as RootStackParamList['Carpark']);
+  const { facilityId } = (route.params as RootStackParamList['Carpark']);
+  const [refreshing, setRefreshing] = useState(false);
   const [spots, setSpots] = useState<number>(0);
   const [total, setTotal] = useState<number>(0);
-  const [refreshing, setRefreshing] = useState(false);
+
+  const pieData = [
+    {value: spots-total, color: 'blue'},
+    {value: spots, color: 'red'},
+  ];
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
@@ -43,16 +48,27 @@ export const CarparkScreen = ({ route }: Props) => {
     fetchCarParks();
   }, [])
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container} edges={['left', 'right', 'bottom']}>
       <ScrollView
         contentContainerStyle={styles.scrollView}
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }>
-        <Text>Pull down to see RefreshControl indicator</Text>
+        }
+      >
+        <View style={styles.pieWrapper}>
+          <Text>{spots - total} / {spots}</Text>
+        </View>
+        <View>
+          <PieChart
+            donut
+            // semiCircle={true}
+            radius={150}
+            innerRadius={130}
+            data={pieData}
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
-    // <Text style={styles.container}>{spots - total} Spots Available</Text>
   );
 }
 
@@ -61,9 +77,12 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   scrollView: {
-    flex: 1,
-    backgroundColor: 'pink',
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pieWrapper: {
+    zIndex: 1,
+    position: 'absolute'
+  }
 });
